e23a2c724c9c9acc916ac5bbe9ba988956d8136d
abufahimkhan <abufahimkhan1@gmail.com>
2024-01-23 22:26:11 +0600

    3rd commit

--- client side/PhoneBook/src/Phonebook.jsx
+++ client side/PhoneBook/src/Phonebook.jsx
@@ -10,10 +10,11 @@ export default function Phonebook() {
       phone: '01704606893',
     },
   ]);
+
   const [firstname, setFirstName] = useState('');
   const [lastname, setLastName] = useState('');
   const [phone, setPhone] = useState('');
-
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   useEffect(() => {
     fetchContacts();
@@ -28,48 +29,62 @@ export default function Phonebook() {
     }
   };
 
-
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if(firstname=='' || lastname=='' || phone==''){
+    if (firstname === '' || lastname === '' || phone === '') {
       alert('Field missing!');
       return;
     }
-    try{
-      await axios.post('http://localhost:3001/saveContact',{
-        firstname,
-        lastname,
-        phone,
-      })
 
-      //now fetching data from MongoDB
+    try {
+      if (selectedContactId) {
+        // If selectedContactId exists, update the contact
+        await axios.put(`http://localhost:3001/updateContact/${selectedContactId}`, {
+          firstname,
+          lastname,
+          phone,
+        });
+      } else {
+        // Otherwise, save a new contact
+        await axios.post('http://localhost:3001/saveContact', {
+          firstname,
+          lastname,
+          phone,
+        });
+      }
+
       fetchContacts();
-      
       setFirstName('');
       setLastName('');
       setPhone('');
-  
-    }catch(err){
-      console.log("Error saving contact", err);
+      setSelectedContactId(null); // Reset selectedContactId after updating or adding
+    } catch (err) {
+      console.log('Error saving contact', err);
     }
   };
 
-  const handleUpdate = (contactId) => {
-  
+  const handleEdit = (contactId) => {
+    // Set the selected contact's data to the state for editing
+    const selectedContact = entries.find((contact) => contact._id === contactId);
+    if (selectedContact) {
+      setFirstName(selectedContact.firstname);
+      setLastName(selectedContact.lastname);
+      setPhone(selectedContact.phone);
+      setSelectedContactId(contactId);
+    }
   };
-  const handleEdit=()=>{}
+
+
+
   const handleDelete = async (contactId) => {
-    // Implement the logic to delete a contact
-    // You will need to make a DELETE request to the server
-    // Update the URL accordingly
     try {
       await axios.delete(`http://localhost:3001/deleteContact/${contactId}`);
-
-      fetchContacts();  // Fetch updated data after deleting contact
+      fetchContacts();
     } catch (err) {
       console.log("Error deleting contact", err);
     }
   };
+
  
  
   return (
@@ -116,15 +131,16 @@ export default function Phonebook() {
               </tr>
             </thead>
             <tbody>
-              {entries.map((info, index) => (
+            {entries.map((info, index) => (
                 <tr key={`${index}`}>
                   <td>{info.firstname}</td>
                   <td>{info.lastname}</td>
                   <td>{info.phone}</td>
-                  <td style={{display:'flex',gap:"10px"}}>
-                    <button style={{backgroundColor:'GrayText'}} onClick={() => handleEdit(info._id)}>Edit</button>
-                    <button style={{backgroundColor:'green'}} onClick={() => handleUpdate(info._id)}>Update</button>
-                    <button style={{backgroundColor:'red'}} onClick={() => handleDelete(info._id)}>Delete</button>
+                  <td>
+                    <button onClick={() => handleEdit(info._id)}>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick={() => handleDelete(info._id)}>Delete</button>
                   </td>
                 </tr>
               ))}
